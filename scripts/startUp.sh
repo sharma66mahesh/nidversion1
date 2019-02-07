5230959b13e04d8d3bb9624273e6b780941ad3fa
sharma66mahesh <gauravpudel1990@gmail.com>
2019-02-07 16:54:41 +0545

    as

--- scripts/startUp.sh
+++ scripts/startUp.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#fix "node" and "forever" not found error on codeDeploy
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
@@ -23,6 +25,7 @@ npm install
 
 rm -rf /home/${USER_NAME}/nid-version-1/web/hfc-key-store
 /home/${USER_NAME}/nid-version-1/web/startFabric.sh
+npm install
 node /home/${USER_NAME}/nid-version-1/web/enrollAdmin.js
 node /home/${USER_NAME}/nid-version-1/web/registerUser.js
 forever start -a -l /home/${USER_NAME}/logs.txt -o /home/${USER_NAME}/output.txt -e /home/${USER_NAME}/error.txt /home/${USER_NAME}/nidversion1/web/app.js
