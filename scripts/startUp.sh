cef2d26263f7bf62e4e16b8b36c3891313f77b07
sharma66mahesh <gauravpudel1990@gmail.com>
2019-02-07 10:48:31 +0545

    as

--- scripts/startUp.sh
+++ scripts/startUp.sh
@@ -15,4 +15,4 @@ sudo rm -rf /home/ec2-user/nid-version-1/web/hfc-key-store
 /home/ec2-user/nid-version-1/web/startFabric.sh
 node /home/ec2-user/nid-version-1/web/enrollAdmin.js
 node /home/ec2-user/nid-version-1/web/registerUser.js
-forever start -l /home/ec2-user/logs.txt -o /home/ec2-user/output.txt -e /home/ec2-user/error.txt /home/ec2-user/nidversion1/web/app.js
+forever start -a -l /home/ec2-user/logs.txt -o /home/ec2-user/output.txt -e /home/ec2-user/error.txt /home/ec2-user/nidversion1/web/app.js
