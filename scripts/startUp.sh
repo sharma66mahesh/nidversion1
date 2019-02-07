3fc84530f336448ccfebb680d23eff36751b79ad
sharma66mahesh <gauravpudel1990@gmail.com>
2019-02-07 17:00:36 +0545

    kpia

--- scripts/startUp.sh
+++ scripts/startUp.sh
@@ -5,9 +5,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-PATH=${PATH}:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/home/${USER_NAME}/.nvm/versions/node/v8.11.2/bin/node
-
 USER_NAME="ubuntu"
+NODE_VERSION="v8.11.2"
+PATH=${PATH}:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/home/${USER_NAME}/.nvm/versions/node/${NODE_VERSION}/bin/node
 sleep 20
 docker stop $(docker ps -q)
 docker rm $(docker ps -qa)
