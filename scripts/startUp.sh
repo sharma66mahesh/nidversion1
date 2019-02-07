f97d23c6fa5a8e17a5df68ba41662db76079111c
sharma66mahesh <gauravpudel1990@gmail.com>
2019-02-07 16:50:39 +0545

    sad

--- scripts/startUp.sh
+++ scripts/startUp.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 PATH=${PATH}:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin
+
 USER_NAME="ubuntu"
 sleep 20
 docker stop $(docker ps -q)
